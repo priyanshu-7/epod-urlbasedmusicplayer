22f9b636afbe044d0c0093b83b0ecb2311e10b82
Priyanshu <priyanshu_s@outlook.com>
2020-10-11 18:58:43 +0530

    initial commit

---
+++ OnlineMusicPlayer/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace OnlineMusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.songList = new System.Windows.Forms.ListBox();
+            this.songName = new System.Windows.Forms.TextBox();
+            this.songURL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.githubBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // songList
+            // 
+            this.songList.BackColor = System.Drawing.Color.Linen;
+            this.songList.Font = new System.Drawing.Font("Montserrat", 10F, System.Drawing.FontStyle.Bold);
+            this.songList.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.songList.FormattingEnabled = true;
+            this.songList.ItemHeight = 23;
+            this.songList.Location = new System.Drawing.Point(610, 2);
+            this.songList.Name = "songList";
+            this.songList.Size = new System.Drawing.Size(288, 372);
+            this.songList.TabIndex = 0;
+            this.songList.SelectedIndexChanged += new System.EventHandler(this.songList_SelectedIndexChanged);
+            // 
+            // songName
+            // 
+            this.songName.Location = new System.Drawing.Point(610, 392);
+            this.songName.Name = "songName";
+            this.songName.Size = new System.Drawing.Size(288, 23);
+            this.songName.TabIndex = 1;
+            this.songName.Text = "Song Name";
+            this.songName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // songURL
+            // 
+            this.songURL.Location = new System.Drawing.Point(610, 423);
+            this.songURL.Name = "songURL";
+            this.songURL.Size = new System.Drawing.Size(288, 23);
+            this.songURL.TabIndex = 2;
+            this.songURL.Text = "Song URL";
+            this.songURL.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(610, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(3, 2);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(601, 444);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Brown;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(797, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(109, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(495, 370);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // githubBtn
+            // 
+            this.githubBtn.BackColor = System.Drawing.Color.Tan;
+            this.githubBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.githubBtn.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.githubBtn.Location = new System.Drawing.Point(3, 454);
+            this.githubBtn.Name = "githubBtn";
+            this.githubBtn.Size = new System.Drawing.Size(601, 38);
+            this.githubBtn.TabIndex = 7;
+            this.githubBtn.Text = "priyanshu-7 @ GitHub";
+            this.githubBtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(900, 496);
+            this.Controls.Add(this.githubBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.songURL);
+            this.Controls.Add(this.songName);
+            this.Controls.Add(this.songList);
+            this.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "E-Pod";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox songList;
+        private System.Windows.Forms.TextBox songName;
+        private System.Windows.Forms.TextBox songURL;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button githubBtn;
+    }
+}
+
---
+++ OnlineMusicPlayer/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections;
+using System.Diagnostics;
+
+namespace OnlineMusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        List<String> listName = new List<String>();
+        List<String> listURL = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+            string constr;
+            constr = "server = HOSTNAME; port = 3306; uid = USERNAME; pwd = PASSWORD; database = DATABASE; OldGuids=True;";
+            conn = new MySqlConnection(constr);
+            conn.Open();
+            MySqlCommand createTable = new MySqlCommand("CREATE TABLE IF NOT EXISTS `music` (" +
+            "`username` VARCHAR(30)," +
+            "`name` VARCHAR(60)," +
+            "`url` TINYTEXT" + ")" , conn);
+            MySqlDataReader reader = createTable.ExecuteReader();
+            conn.Close();
+            //Console.WriteLine("Connection Open!");
+            conn.Open();
+            string sql = "SELECT * FROM music";
+            var cmd = new MySqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                songList.Items.Add(reader.GetString(1));
+                listName.Add(reader.GetString(1));
+                listURL.Add(reader.GetString(2));
+            }
+            conn.Close();
+
+        }
+        private void songName_Click(object sender, EventArgs e)
+        {
+            if(songName.Text == "Song Name")
+            {
+                songName.Text = "";
+            }
+        }
+        private void songURL_Click(object sender, EventArgs e)
+        {
+            if (songName.Text == "Song Name")
+            {
+                songName.Text = "";
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void songList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = listURL[songList.SelectedIndex];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String name = songName.Text;
+            String url = songURL.Text;
+            if (String.IsNullOrWhiteSpace(name) || String.IsNullOrWhiteSpace(url))
+            {
+                string title = "Error";
+                string message = "Please enter both the name and the URL";
+                MessageBox.Show(message, title);
+            }
+            else
+            {
+                
+                //add to db, open then close conn
+                string last3 = url.Substring(url.Length - 4);
+                if (last3.ToLower() == ".mp3" || last3.ToLower() == ".m4a")
+                {
+                    songList.Items.Add(name);
+                    listName.Add(name);
+                    listURL.Add(url);
+                    conn.Open();
+                    String query = "INSERT INTO music(username,name,url) VALUES (@username,@name, @url)";
+                    MySqlCommand command = new MySqlCommand(query, conn);
+                    command.Parameters.Add("@username", MySqlDbType.VarString).Value = "test";
+                    command.Parameters.Add("@name", MySqlDbType.VarString).Value = name;
+                    command.Parameters.Add("@url", MySqlDbType.VarString).Value = url;
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                }
+                else
+                {
+                    string title = "Invalid URL";
+                    string message = "Only .mp3/.m4a allowed";
+                    MessageBox.Show(message, title);
+                }
+            }
+        }
+
+        public void updateToNext(int index)
+        {
+            int next_index = (index + 1)%songList.Items.Count;
+            songList.SelectedIndex = next_index;
+            axWindowsMediaPlayer1.URL = listURL[next_index];
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int indexToDelete = songList.SelectedIndex;
+            String name = listName[indexToDelete];
+            if (!name.Contains("DEFAULT"))
+            {
+                updateToNext(songList.SelectedIndex);
+                listName.RemoveAt(indexToDelete);
+                listURL.RemoveAt(indexToDelete);
+                songList.Items.RemoveAt(indexToDelete);
+                conn.Open();
+                String query = "DELETE FROM music WHERE name = @name AND username = @username";
+                MySqlCommand command = new MySqlCommand(query, conn);
+                command.Parameters.Add("@name", MySqlDbType.VarString).Value = name;
+                command.Parameters.Add("@username", MySqlDbType.VarString).Value = "test";
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            else
+            {
+                string title = "Error";
+                string message = "You cannot delete the default radio stream.";
+                MessageBox.Show(message, title);
+            }
+        }
+        private void githubBtn_Click(Object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/priyanshu-7");
+        }
+
+    }
+}
